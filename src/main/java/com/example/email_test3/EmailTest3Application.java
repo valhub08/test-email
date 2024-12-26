b1f326ed524bac4c8134ee524218017477abbbee
valhub08 <jhstl1203@gmail.com>
2024-12-26 20:45:31 +0900

    feat :: EmailTest3Application

---
+++ src/main/java/com/example/email_test3/EmailTest3Application.java
@@ -0,0 +1,13 @@
+package com.example.email_test3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailTest3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailTest3Application.class, args);
+    }
+
+}
