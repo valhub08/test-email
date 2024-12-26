11ba0be7ad00f05413110673c59cba210d312ada
valhub08 <jhstl1203@gmail.com>
2024-12-26 20:44:46 +0900

    feat :: ApiResponse

---
+++ src/main/java/com/example/email_test3/dto/response/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.example.email_test3.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+    private final boolean success;
+    private final String message;
+    private final T data;
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> success(String message) {
+        return success(message, null);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+}
